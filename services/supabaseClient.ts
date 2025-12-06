40114505196199816e3f0118b36b0e6a04dd67ca
cormacgwin <cormacgoodwin@gmail.com>
2025-12-06 14:38:50 -0500

    Add files via upload

--- services/supabaseClient.ts
+++ services/supabaseClient.ts
@@ -1,8 +1,8 @@
 
 import { createClient } from '@supabase/supabase-js';
 
-// I extracted this ID from the API Key you shared!
-const SUPABASE_URL = 'https://gfvibzsysaskgyhplrvd.supabase.co';
+// Updated with the correct Project URL
+const SUPABASE_URL = 'https://gfvebzsysaskgyhplrvd.supabase.co';
 
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdmdmVienN5c2Fza2d5aHBscnZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUwMjk3MjcsImV4cCI6MjA4MDYwNTcyN30.UOhulRCr7qhBkGGem4Fu-0ku70wXqnILHPLVwRoI_5A';
 
